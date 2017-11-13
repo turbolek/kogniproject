2c446815b5b9a8d0a3c42294b1dbcaace7592683
Daniel Kunicki <dankunicki@gmail.com>
2017-11-13 23:30:20 +0100

    Add unit spawner and temporary spawn buttons

---
+++ kogniproject/Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public bool owner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnUnit(Transform unitPrefab)
+    {
+        Transform unit = Instantiate(unitPrefab, gameObject.transform.position, Quaternion.identity);
+        unit.parent = gameObject.transform;
+    
+    }
+}
