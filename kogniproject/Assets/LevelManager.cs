ad9bdcd4d93ee4997bcfaaba073da352ad595393
Daniel Kunicki <dankunicki@gmail.com>
2018-01-14 21:54:11 +0100

    Add main menu

---
+++ kogniproject/Assets/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            } else
+            {
+                LoadScene("Menu");
+            }
+        }
+    }
+}
---
+++ kogniproject/Assets/MenuManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    [SerializeField]
+
+    private Button[] _buttons;
+    private int _activeButtonIndex = 0;
+
+    private void Start()
+    {
+        HighlightButton(_buttons[_activeButtonIndex]);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            DehighlightButton(_buttons[_activeButtonIndex]);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _activeButtonIndex--;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                _activeButtonIndex++;
+            }
+            _activeButtonIndex = Mathf.Clamp(_activeButtonIndex, 0, _buttons.Length - 1);
+            HighlightButton(_buttons[_activeButtonIndex]);
+
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                _buttons[_activeButtonIndex].onClick.Invoke();
+            }
+        }
+
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        LevelManager levelManager = FindObjectOfType<LevelManager>();
+        levelManager.LoadScene(levelName);
+    }
+
+    private void HighlightButton(Button button)
+    {
+        button.GetComponentInChildren<Text>().fontSize = 40;
+    }
+
+    private void DehighlightButton(Button button)
+    {
+        button.GetComponentInChildren<Text>().fontSize = 30;
+    }
+}
